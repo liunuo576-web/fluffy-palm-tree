77132d7cf74a1f9cc89fa006bb976c60280357db
liunuo576-web <liunuo576@gmail.com>
2026-05-31 00:20:44 +0800

    fix: 添加 base 路径配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/fluffy-palm-tree/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
